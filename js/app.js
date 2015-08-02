0484762bcec5928923e15fbd813e49b15260a5bf
Tony Norman <tonykodiac@gmail.com>
2015-08-02 17:51:18 -0500

    first jq entres

--- js/app.js
+++ js/app.js
@@ -1,11 +1,38 @@
 $(document).ready(function() {
 
  $('.ryu').mouseenter(function() {
-    alert('mouse entered .ryu div');
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+
+
+
+
   });
 
+ $('.ryu') .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  });
+
+    $('.ryu').mousedown(function() {    
+     // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+    // animate hadouken to the right of the screen
+    // play hadouken sound
+    // show hadouken and animate it to the right of the screen
+  })
+
+ $('.ryu').mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+
+   $('.ryu-throwing').hide();
+   $('.ryu-ready').show();
 
 
-	
 
 });
+
