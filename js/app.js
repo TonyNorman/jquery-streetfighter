ef63df0a5d520929f55868ca8c4f5e75581f6690
Tony Norman <tonykodiac@gmail.com>
2015-08-05 22:09:13 -0500

    cool

--- js/app.js
+++ js/app.js
@@ -41,25 +41,45 @@ $(document).ready(function() {
     // ryu goes back to his ready position
   });
 
-//  
-$(this).keydown(function(event){
-     event.preventDefault();
-     if(event.keyCode===88){
-      playHadouken();
-       $('.ryu-ready').hide();
-      $('.ryu-throwing').show();
-      $('.hadouken').show().animate(
-        {'left': '1020px'},
-        500,
-        function() {
-         $('.ryu-throwing').hide();
-         // $('.hadouken').css('left', '520px');
-         $('.hadouken').hide();
-         $('.ryu-ready').show();  
-     });
-    }
+ 
+// $(this).keydown(function(event){
+//      event.preventDefault();
+//      if(event.keyCode===88){
+//       playHadouken();
+//        $('.ryu-ready').hide();
+//       $('.ryu-throwing').show();
+//       $('.hadouken').show().animate(
+//         {'left': '1020px'},
+//         500,
+//         function() {
+//          $('.ryu-throwing').hide();
+//          // $('.hadouken').css('left', '520px');
+//          $('.hadouken').hide();
+//          $('.ryu-ready').show();  
+//      });
+//     }
 
-});
+// });
+
+  $(this).keydown(function(event)){
+   if (event.keyCode===88) {
+
+      $('ryu-ready').hide();
+      $('ryu-cool').show();
+     }
+
+
+     });
+
+
+
+
+
+
+
+
+
+
 
 
 
